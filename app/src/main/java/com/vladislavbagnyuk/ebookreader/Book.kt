1807ed76003eded25fc6b1c6622e4f61c27d12a6
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-21 18:10:00 +0100

    Book class

---
+++ app/src/main/java/com/vladislavbagnyuk/ebookreader/Book.kt
@@ -0,0 +1,16 @@
+package com.vladislavbagnyuk.ebookreader
+
+data class Book(val title: String, val author: String, val cover: Int, val pages: Int, val lastPage: Int, val content: String) {
+    fun percentage() : Int {
+        return ((lastPage.toDouble() / pages) * 100).toInt()
+    }
+}
+
+fun getSampleHabits(): List<Book> {
+    return listOf(
+        Book("1984", "George Orwell", R.drawable.cover1984, 860, 320, "Lorem ipsum dolor sit amet"),
+        Book("The Lord of the Rings: The Fellowship of the Ring", "J. R. R. Tolkien", R.drawable.coverLOTR, 1200, 200, "Lorem ipsum dolor sit amet"),
+        Book("Harry Potter and the Half-Blood Prince", "J. K. Rowling", R.drawable.coverHarryPotter, 500, 320, "Lorem ipsum dolor sit amet"),
+        Book("1984", "George Orwell", R.drawable.cover1984, 860, 320, "Lorem ipsum dolor sit amet")
+    )
+}
