dc6f98ad646e589906049f26d3790eef4bea25f0
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-21 18:49:46 +0100

    Fixed images & cleaned up layout

--- app/src/main/java/com/vladislavbagnyuk/ebookreader/Book.kt
+++ app/src/main/java/com/vladislavbagnyuk/ebookreader/Book.kt
@@ -9,8 +9,8 @@ data class Book(val title: String, val author: String, val cover: Int, val pages
 fun getSampleHabits(): List<Book> {
     return listOf(
         Book("1984", "George Orwell", R.drawable.cover1984, 860, 320, "Lorem ipsum dolor sit amet"),
-        Book("The Lord of the Rings: The Fellowship of the Ring", "J. R. R. Tolkien", R.drawable.coverLOTR, 1200, 200, "Lorem ipsum dolor sit amet"),
-        Book("Harry Potter and the Half-Blood Prince", "J. K. Rowling", R.drawable.coverHarryPotter, 500, 320, "Lorem ipsum dolor sit amet"),
+        Book("The Lord of the Rings: The Fellowship of the Ring", "J. R. R. Tolkien", R.drawable.coverlotr, 1200, 200, "Lorem ipsum dolor sit amet"),
+        Book("Harry Potter and the Half-Blood Prince", "J. K. Rowling", R.drawable.cover_harry_potter, 500, 320, "Lorem ipsum dolor sit amet"),
         Book("1984", "George Orwell", R.drawable.cover1984, 860, 320, "Lorem ipsum dolor sit amet")
     )
 }
