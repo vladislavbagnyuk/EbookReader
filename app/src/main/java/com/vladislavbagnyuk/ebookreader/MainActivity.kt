3d556ce7879d8126c3bcea64e93dc362ab7664e4
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-22 20:04:52 +0100

    Continue reading section & status bar customization

--- app/src/main/java/com/vladislavbagnyuk/ebookreader/MainActivity.kt
+++ app/src/main/java/com/vladislavbagnyuk/ebookreader/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.vladislavbagnyuk.ebookreader
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TableLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,32 +14,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        //setSupportActionBar(findViewById(R.id.toolbar))
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
 
+        rv_recent.setHasFixedSize(true)
+        rv_recent.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rv_recent.adapter = RecentAdapter(getRecentBooks())
+
         rv_library.setHasFixedSize(true)
-        rv_library.layoutManager = GridLayoutManager(this, 2);
-        rv_library.adapter = BooksAdapter(getSampleBooks())
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        rv_library.layoutManager = GridLayoutManager(this, 2)
+        rv_library.adapter = LibraryAdapter(getSampleBooks())
     }
 }
 
@@ -64,4 +50,12 @@ fun getSampleBooks(): List<Book> {
             Book("Harry Potter and the Half-Blood Prince", "J. K. Rowling", R.drawable.cover_harry_potter, 500, 320, "Lorem ipsum dolor sit amet"),
             Book("1984", "George Orwell", R.drawable.cover1984, 860, 320, "Lorem ipsum dolor sit amet"),
     )
+}
+
+fun getRecentBooks(): List<Book> {
+    return listOf(
+        Book("1984", "George Orwell", R.drawable.cover1984, 860, 320, "Lorem ipsum dolor sit amet"),
+        Book("The Lord of the Rings: The Fellowship of the Ring", "J. R. R. Tolkien", R.drawable.coverlotr, 1200, 200, "Lorem ipsum dolor sit amet"),
+        Book("Harry Potter and the Half-Blood Prince", "J. K. Rowling", R.drawable.cover_harry_potter, 500, 320, "Lorem ipsum dolor sit amet"),
+        Book("1984", "George Orwell", R.drawable.cover1984, 860, 320, "Lorem ipsum dolor sit amet"),)
 }
---
+++ app/src/main/java/com/vladislavbagnyuk/ebookreader/RecentAdapter.kt
@@ -0,0 +1,34 @@
+package com.vladislavbagnyuk.ebookreader
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.book_card_library.view.*
+
+class RecentAdapter(private val books: List<Book>) :
+        RecyclerView.Adapter<RecentAdapter.BookViewHolder>() {
+
+    class BookViewHolder(val card: View) : RecyclerView.ViewHolder(card)
+
+    // Create a new ViewHolder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.book_card_recent, parent, false)
+        return BookViewHolder(view)
+    }
+
+    // Specifies the contents for the shown book card
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: BookViewHolder, index: Int) {
+        val book = books[index]
+        with(holder.card) {
+            iv_cover.setImageResource(book.cover)
+            tv_percentage.text = "${book.percentage()}%"
+        }
+
+    }
+
+    override fun getItemCount() = books.size
+
+}
