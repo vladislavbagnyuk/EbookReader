bfecf4354f3dd4ee502b1fc506f843abe792f25e
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-27 11:27:09 +0100

    Showing & hiding appbar in book activity

--- app/src/main/java/com/vladislavbagnyuk/ebookreader/BookActivity.kt
+++ app/src/main/java/com/vladislavbagnyuk/ebookreader/BookActivity.kt
@@ -1,11 +1,24 @@
 package com.vladislavbagnyuk.ebookreader
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_book.*
 
 class BookActivity : AppCompatActivity() {
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book)
+
+    }
+
+    fun showControls(view: View) {
+        if (toolbar.visibility == View.GONE) {
+            toolbar.visibility = View.VISIBLE
+        } else {
+            toolbar.visibility = View.GONE
+        }
     }
 }
--- app/src/main/java/com/vladislavbagnyuk/ebookreader/LibraryAdapter.kt
+++ app/src/main/java/com/vladislavbagnyuk/ebookreader/LibraryAdapter.kt
@@ -9,24 +9,22 @@ import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.book_card_library.view.*
 
 class LibraryAdapter(private val books: List<Book>) :
-        RecyclerView.Adapter<LibraryAdapter.BookViewHolder>() {
+    RecyclerView.Adapter<LibraryAdapter.BookViewHolder>() {
 
     class BookViewHolder(val card: View) : RecyclerView.ViewHolder(card)
 
     // Create a new ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.book_card_library, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.book_card_library, parent, false)
         return BookViewHolder(view)
+
     }
 
     // Specifies the contents for the shown book card
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: BookViewHolder, index: Int) {
         val book = books[index]
-
-        val options = BitmapFactory.Options().apply {
-            inJustDecodeBounds = true
-        }
 
         with(holder.card) {
             iv_cover.setImageResource(book.cover)
