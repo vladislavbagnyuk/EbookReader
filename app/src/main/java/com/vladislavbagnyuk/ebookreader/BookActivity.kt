0d47a36ac7ae2755242fae41afde8cc26388c5ed
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-28 15:08:19 +0100

    BottomAppBar animation

--- app/src/main/java/com/vladislavbagnyuk/ebookreader/BookActivity.kt
+++ app/src/main/java/com/vladislavbagnyuk/ebookreader/BookActivity.kt
@@ -1,9 +1,10 @@
 package com.vladislavbagnyuk.ebookreader
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomappbar.BottomAppBar
 import kotlinx.android.synthetic.main.activity_book.*
 
 class BookActivity : AppCompatActivity() {
@@ -11,17 +12,24 @@ class BookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book)
+        bottomAppBar.post {
+            bottomAppBar.performHide()
+        }
     }
 
     fun showControls(view: View) {
         if (toolbar.visibility == View.GONE) {
+            bottomAppBar.performShow()
             toolbar.visibility = View.VISIBLE
         } else {
             toolbar.visibility = View.GONE
+            bottomAppBar.performHide()
         }
+
     }
 
     fun back(view: View) {
         finish()
     }
 }
+
