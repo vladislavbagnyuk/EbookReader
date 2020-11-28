c8a419229e332540cf185881b3f92990194a5a3b
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-28 17:14:16 +0100

    Basic bottom panel

--- app/src/main/java/com/vladislavbagnyuk/ebookreader/BookActivity.kt
+++ app/src/main/java/com/vladislavbagnyuk/ebookreader/BookActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.bottomappbar.BottomAppBar
 import kotlinx.android.synthetic.main.activity_book.*
 
 class BookActivity : AppCompatActivity() {
