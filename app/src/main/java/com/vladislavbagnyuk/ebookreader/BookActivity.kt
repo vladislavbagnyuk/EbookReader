92506dc3daecfae99229ea6573f8902b75ded8c2
vladislavbagnyuk <vladislav.bagnyuk@gmail.com>
2020-11-30 15:05:49 +0100

    Spinner & 1 button

--- app/src/main/java/com/vladislavbagnyuk/ebookreader/BookActivity.kt
+++ app/src/main/java/com/vladislavbagnyuk/ebookreader/BookActivity.kt
@@ -3,6 +3,7 @@ package com.vladislavbagnyuk.ebookreader
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_book.*
 
@@ -14,6 +15,19 @@ class BookActivity : AppCompatActivity() {
         bottomAppBar.post {
             bottomAppBar.performHide()
         }
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.planets_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            font_spinner.adapter = adapter
+        }
+
     }
 
     fun showControls(view: View) {
@@ -24,7 +38,6 @@ class BookActivity : AppCompatActivity() {
             toolbar.visibility = View.GONE
             bottomAppBar.performHide()
         }
-
     }
 
     fun back(view: View) {
